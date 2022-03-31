28dd6d90814ea61e68c670f8b04264e6b232a14e
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-03-31 08:21:42 +0200

    user reg and session

--- Rails/scheduled_tweets/config/routes.rb
+++ Rails/scheduled_tweets/config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "about", to: "about#index"   
   get "sign_up", to: "regesteration#new" 
-  post "sign_up", to: "regesteration#create"
+  post "sign_up", to: "regesteration#create" 
+  
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "sign_out", to: "sessions#destroy"
   root to: "main#index"
 end
