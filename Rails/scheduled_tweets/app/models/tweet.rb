5c5bd3c9f2654cd529e1ef96f8beaa62e066221d
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-05-10 22:20:24 +0200

    schedule tweets

--- Rails/scheduled_tweets/app/models/tweet.rb
+++ Rails/scheduled_tweets/app/models/tweet.rb
@@ -1,4 +1,16 @@
 class Tweet < ApplicationRecord
   belongs_to :twitter_account 
-  belongs_to :user
+  belongs_to :user 
+
+  validates :body, length: {minimum: 1, maximum: 280 } 
+  validates :publish_at, presence: true 
+
+  after_initialize do 
+    self.publish_at ||= 24.hour.from_now
+  end  
+
+  def published? 
+    tweet_id? 
+  end 
+  
 end
