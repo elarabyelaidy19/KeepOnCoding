11023cbdcc104b25e49e3e1bf1b14bf9b77df4c1
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-05-22 19:42:00 +0200

    unit test model

---
+++ Rails/nps/Testing.md
@@ -0,0 +1,3 @@
+# Testing 
+- Think of all test cases and write them down write test make it fails. write code to make it pass. and loop it.
+- 
--- Rails/nps/test/models/rating_test.rb
+++ Rails/nps/test/models/rating_test.rb
@@ -23,5 +23,52 @@ class RatingTest < ActiveSupport::TestCase
     rating = Rating.new(score: -1) 
     rating.valid? 
     assert_not rating.errors[:score].empty?
+  end  
+
+  test "promoter? return false if acore less than 9" do 
+    rating = Rating.new(score: 8) 
+    assert_not rating.promoter?
+  end   
+
+  test "promoter? return false if score greater than 10" do 
+    rating = Rating.new(score: 11) 
+    assert_not rating.promoter?
   end 
+
+
+  test "passive? return true if score is 7 or 8" do 
+    7.upto(8).each do |i| 
+      rating = Rating.new(score: i) 
+      assert rating.passive?
+    end 
+  end 
+
+  test "passive? return false if score is less than 7" do 
+    0.upto(6).each do |i| 
+      rating = Rating.new(score: i) 
+      assert_not rating.passive?
+    end 
+  end  
+
+  test "passive? return false if score is greater than 8" do 
+    9.upto(10).each do |i| 
+      rating = Rating.new(score: i) 
+      assert_not rating.passive? 
+    end 
+  end 
+  
+  test "detractor? return true if score is less than or equla 6" do  
+    0.upto(6).each do |i| 
+      rating = Rating.new(score: i) 
+      assert rating.detractor? 
+    end
+  end
+
+  test "detractor? return false if score is greater than 6" do 
+    7.upto(10).each do |i| 
+      rating = Rating.new(score: i) 
+      assert_not rating.detractor? 
+    end 
+  end 
+
 end
