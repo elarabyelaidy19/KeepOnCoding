568c5c9bd03083b2973716d11e1ad387eb658f13
elarabyelaidy19 <elarabyelaidy@gmail.com>
2023-12-31 00:38:36 +0200

    simple websocket app

---
+++ Rails/websock/app/controllers/hangouts_controller.rb
@@ -0,0 +1,9 @@
+class HangoutsController < ApplicationController
+  
+  before_action :authenticate_user!
+  
+  def index 
+     @message = Message.new  
+     @messages = Message.includes(:user)
+  end
+end
